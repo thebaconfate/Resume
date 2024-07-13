0b19bcffd434542c85614c9d0da269c9c1e4cdcd
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-07-13 23:02:52 +0200

    WIP

--- resume/__main__.py
+++ resume/__main__.py
@@ -1,17 +1,18 @@
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.pagesizes import A4
-from reportlab.lib.colors import Color
+from reportlab.lib.colors import Color, blue
 from resume.border import Border
 
 
 c = Canvas("hello.pdf")
-left_border = Color(37, 150, 190)
+left_border = Color(37 / 255, 150 / 255, 190 / 255)
 border = Border(*A4)
 print(border.min_width, border.min_height)
 print(border.max_width, border.max_height)
 print(border.width, border.height)
 
 c.drawString(border.min_width, border.min_height, "Hello, World!")
-c.rect(0, 0, border.min_width * 2, border.height, fill=left_border)
+c.setFillColor(left_border)
+c.rect(0, 0, border.min_width * 2, border.height, fill=True, stroke=False)
 c.showPage()
 c.save()
